1901ccfa30339894ce944c4cb2fe011bba9c1c77
Lucas Correa <lucas.correa@etaure.srv>
2021-05-06 21:33:53 -0300

    Angular parte 1 (completo)

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+
+import { PhotoModule } from "./photo/photo.module";
+import { PhotoListModule } from "./photo-list/photo-list.module";
+import { PhotoFormModule } from "./photo-form/photo-form.module";
 
 @NgModule({
-  declarations: [ PhotoComponent ],
-  exports: [ PhotoComponent ]
+  imports:
+  [
+    PhotoModule,
+    PhotoListModule,
+    PhotoFormModule
+  ]
 })
-export class PhotosModule {
-
-}
+export class PhotosModule {}
