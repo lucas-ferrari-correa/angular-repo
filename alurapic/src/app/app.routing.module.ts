e70fec5966158e1bc2e59c80732fa66fbd22be8f
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2021-05-09 12:52:47 -0300

    Angular parte 2 completo

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -5,14 +5,16 @@ import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
-import { SignInComponent } from "./home/signin/signin.component";
-import { AuthGuard } from "./core/auth/auth.guard";
 
 const routes: Routes = [
   {
     path: '',
-    component: SignInComponent,
-    canActivate: [AuthGuard]
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomeModule'
   },
   {
     path: 'user/:userName',
--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignUpComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home.component";
+import { HomeRoutingModule } from "./home.routing.module";
 
 @NgModule({
-  declarations: [ SignInComponent ],
+  declarations: [
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent
+  ],
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     CommonModule,
     VMessageModule,
-    RouterModule
+    RouterModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule {
