66183eb7f9920e31f1264c4b2f7963a52a8e5a88
Lucas <lucasfcorrea2206@gmail.com>
2021-07-07 16:21:07 -0300

    Curso de Angular Alura completo

--- alurapic-2/src/app/home/singup/singup.component.ts
+++ alurapic-2/src/app/home/singup/singup.component.ts
@@ -12,10 +12,10 @@ import { PlatformDetectorService } from '../../core/plataform-detector/platform-
     providers: [ UserNotTakenValidatorService ]
 })
 export class SignUpComponent implements OnInit {
-    
+
     signupForm: FormGroup;
     @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
-    
+
     constructor(
         private formBuilder: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService,
@@ -25,20 +25,20 @@ export class SignUpComponent implements OnInit {
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
-            email: ['', 
+            email: ['',
                 [
                     Validators.required,
                     Validators.email
                 ]
             ],
-            fullName: ['', 
+            fullName: ['',
                 [
                     Validators.required,
                     Validators.minLength(2),
                     Validators.maxLength(40)
                 ]
             ],
-            userName: ['', 
+            userName: ['',
                 [
                     Validators.required,
                     lowerCaseValidator,
@@ -47,7 +47,7 @@ export class SignUpComponent implements OnInit {
                 ],
                 this.userNotTakenValidatorService.checkUserNameTaken()
             ],
-            password: ['', 
+            password: ['',
                 [
                     Validators.required,
                     Validators.minLength(8),
@@ -56,11 +56,12 @@ export class SignUpComponent implements OnInit {
             ]
         });
 
-        this.platformDetectorService.isPlatformBrowser() && 
-            this.emailInput.nativeElement.focus();    
-    } 
+        this.platformDetectorService.isPlatformBrowser() &&
+            this.emailInput.nativeElement.focus();
+    }
 
     signup() {
+      if (this.signupForm.invalid && this.signupForm.pending) {
         const newUser = this.signupForm.getRawValue() as NewUser;
         this.signUpService
             .signup(newUser)
@@ -68,5 +69,6 @@ export class SignUpComponent implements OnInit {
                 () => this.router.navigate(['']),
                 err => console.log(err)
             );
+      }
     }
 }
