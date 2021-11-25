1d00bf5eb25f949a337853aa6b149ac0bb6abd8c
Lucas <lucasfcorrea2206@gmail.com>
2021-11-25 08:58:56 -0300

    Angular começando o framework 1.2 completo

---
+++ bytebank2/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+  valor: number = 12;
+  destino: number = 222;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+  }
+}
